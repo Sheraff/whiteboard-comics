00375e74858a1210f0e9bbde1075fad7eae90447
Sheraff <fpellet@ensc.fr>
2015-11-22 20:58:50 +0100

    more pretty svg and php includes

--- index.php
+++ index.php
@@ -2,44 +2,10 @@
 <?
 	$master = $_GET['all'];
 
-	include 'php_utils.php';
-
-	// set TIMEZONE
-	date_default_timezone_set('America/Los_Angeles');
-	$time = time();
+	require 'php_utils.php';
 
 	// read METADATA
-	$graphs = array();
-	$metadata = str_getcsv(file_get_contents('graph_list.tsv'), "\n");
-	foreach($metadata as $key => &$row){
-		$row = str_getcsv($row, "\t");
-		if($key===0)
-			continue;
-		$temp = $row;
-		$row = [];
-		foreach($temp as $key => $item)
-			$row[$metadata[0][$key]] = $item;
-		$row[timestamp] = strtotime("$row[release] 8:45:00");
-		$row[release] = explode('-', $row[release]);
-		$row[path] = "graphs/graphs_$row[name].svg";
-		if(file_exists("png/$row[name].png"))
-				$row[watermarked] = "png/$row[name].png";
-		$row[thumbnail] = $row[watermarked] ? $row[watermarked] : "thumb/graphs_$row[name].png";
-		$row[tags] = str_getcsv($row[tags]);
-		$row[formatted_name] = trim( ucwords( preg_replace('/_/', ' ', preg_replace('/,/', ', ', preg_replace('/([=\(\)])/', ' $1 ', $row[name]) ) ) ) );
-		$row[content] = false;
-
-		if(file_exists($row[path]) && ($row[timestamp] < $time || $master)) // TIME. Release time is at 8:45am Los Angeles time (11:45am NYC, 5:45pm Paris)
-			$graphs[] = $row;
-	}
-
-	// order graphs // debug: this shouldn't need to happen
-	function anti_chronological($a, $b){
-		if($a[timestamp]===$b[timestamp])
-			return 0;
-		return $a[timestamp] > $b[timestamp] ? -1 : 1;
-	}
-	usort($graphs, 'anti_chronological');
+	$graphs = read_metadata();
 
 	// try and MATCH graph to query
 	$initial_index = -1;
@@ -53,7 +19,6 @@
 	} else{
 		echo "use matched name $initial_index\n";
 	}
-	// $graphs[$initial_index][content] = file_get_contents($graphs[$initial_index][path]); // load content for matched (or latest) graph
 
 	// create CONTENT bites
 	// links
@@ -78,7 +43,7 @@
 			$letter
 		);
 		$letters[] = [
-			'content' => file_get_contents($file),
+			'content' => format_alphabet(file_get_contents($file)),
 			'letter' => $letter
 		];
 	}
@@ -112,7 +77,9 @@
 	<link href='style.css' rel='stylesheet'>
 </head>
 <style>
-
+	main svg{
+		max-width: 100%;
+	}
 </style>
 <noscript><img src="http://whiteboard-comics.com/<? echo $thumbnail; ?>" alt="<? echo $formatted_name; ?>" /></noscript>
 <aside>
--- php_utils.php
+++ php_utils.php
@@ -1,4 +1,44 @@
 <?
+  // set TIMEZONE
+  date_default_timezone_set('America/Los_Angeles');
+
+
+  function anti_chronological($a, $b){
+    if($a[timestamp]===$b[timestamp])
+      return 0;
+    return $a[timestamp] > $b[timestamp] ? -1 : 1;
+  }
+
+  function read_metadata(){
+    $time = time();
+    $graphs = array();
+    $metadata = str_getcsv(file_get_contents('graph_list.tsv'), "\n");
+    foreach($metadata as $key => &$row){
+      $row = str_getcsv($row, "\t");
+      if($key===0)
+        continue;
+      $temp = $row;
+      $row = [];
+      foreach($temp as $key => $item)
+        $row[$metadata[0][$key]] = $item;
+      $row[timestamp] = strtotime("$row[release] 8:45:00");
+      $row[release] = explode('-', $row[release]);
+      $row[path] = "graphs/graphs_$row[name].svg";
+      if(file_exists("png/$row[name].png"))
+          $row[watermarked] = "png/$row[name].png";
+      $row[thumbnail] = $row[watermarked] ? $row[watermarked] : "thumb/graphs_$row[name].png";
+      $row[tags] = str_getcsv($row[tags]);
+      $row[formatted_name] = trim( ucwords( preg_replace('/_/', ' ', preg_replace('/,/', ', ', preg_replace('/([=\(\)])/', ' $1 ', $row[name]) ) ) ) );
+      $row[content] = false;
+
+      if(file_exists($row[path]) && ($row[timestamp] < $time || $master)) // TIME. Release time is at 8:45am Los Angeles time (11:45am NYC, 5:45pm Paris)
+        $graphs[] = $row;
+    }
+    // order graphs // debug: this shouldn't need to happen
+    usort($graphs, 'anti_chronological');
+    return $graphs;
+  }
+
   function format_svg($string){
     $string = preg_replace([
 			'/<\?xml.*?>/',
@@ -28,6 +68,39 @@
 			' ',
 			' ',
 			'd="M'
+		], $string);
+		return $string;
+  }
+
+  function format_alphabet($string){
+    $string = preg_replace([
+      '/<\?xml.*?>/',
+      '/<!--.*?-->/',
+      '/<!DOCTYPE.*?>/',
+      '/<svg.*?viewBox="(([0-9\.]+ ?){4})".*?>/',
+      '/ ?stroke-linejoin="round" ?/',
+      '/ ?stroke-linejoin="bevel" ?/',
+      '/ ?stroke-linecap="round" ?/',
+      '/ ?stroke-width="4" ?/',
+      '/ ?fill="none" ?/',
+      '/ ?stroke="#(0[0-1]){3}" ?/',
+      '/ ?stroke-miterlimit="10" ?/',
+      '/ +/',
+      '/d=" M/'
+    ], [
+      ' ',
+      ' ',
+      ' ',
+      '<svg viewBox="$1">',
+      ' ',
+      ' ',
+      ' ',
+      ' ',
+      ' ',
+      ' ',
+      ' ',
+      ' ',
+      'd="M'
 		], $string);
 		return $string;
   }
