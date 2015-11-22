334d5f8f67ecad7701c6d78b904435a8a4a1d2a6
Sheraff <fpellet@ensc.fr>
2015-11-22 09:18:41 +0100

    patched temporary archives for deployment

--- archives/index.php
+++ archives/index.php
@@ -9,8 +9,7 @@
 	<meta name="author" content="Florian Pellet">
 	<meta name="language" content="en">
 	<link rel="icon" type="image/png" href="favicon.png">
-	<link href='http://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
 	<link href='../style.css' rel='stylesheet'>
 	<style>
 		svg{
@@ -22,9 +21,24 @@
 		}
 		svg [data-type="erase"]{
 			visibility: hidden;
+			stroke: white;
+			stroke-linejoin: bevel;
 		}
 		svg text{
-			fill: white;
+			fill: black;
+			font-family: 'Permanent Marker', 'Chalkboard', 'Comic Sans MS', Arial;
+		}
+		svg path, svg line, svg polyline{
+			stroke-linecap: round;
+			stroke-linejoin: round;
+			stroke-miterlimit: 10;
+			fill: none;
+		}
+		svg path:not([stroke]), svg line:not([stroke]), svg polyline:not([stroke]){
+			stroke: black;
+		}
+		svg path:not([stroke-width]), svg line:not([stroke-width]), svg polyline:not([stroke-width]){
+			stroke-width: 4;
 		}
 		.container{
 			position: relative;
@@ -55,43 +69,43 @@
 </head>
 
 <?
+	// set TIMEZONE
 	date_default_timezone_set('America/Los_Angeles');
+	$time = time();
 
-	function path_to_components($path){
-		$name_pieces = explode('-', basename($path), 4);
-
-		$tags = explode('$', substr($name_pieces[3], 0, -4));
-		if(count($tags)>1)
-			$name = array_shift($tags);
-		else{
-			$name = $tags[0];
-			$tags = false;
-		}
-
-		return [
-			'path' => $path,
-			'name' => $name,
-			'release_date_pieces' => [$name_pieces[0], $name_pieces[1], $name_pieces[2]],
-			'release_date' => strtotime("$name_pieces[0]-$name_pieces[1]-$name_pieces[2] 8:45:00"),
-			'thumbnail' => str_replace(['graphs/', '.svg', '$'], ['png/', '.png', '#'], $path),
-			'formatted_name' => trim( ucwords( preg_replace('/_/', ' ', preg_replace('/,/', ', ', preg_replace('/([=\(\)])/', ' $1 ', $name) ) ) ) ),
-			'tags' => $tags
-		];
+	// read METADATA
+	$graphs = array();
+	$metadata = str_getcsv(file_get_contents('../graph_list.tsv'), "\n");
+	foreach($metadata as $key => &$row){
+		$row = str_getcsv($row, "\t");
+		if($key===0)
+			continue;
+		$temp = $row;
+		$row = [];
+		foreach($temp as $key => $item)
+			$row[$metadata[0][$key]] = $item;
+		$row[timestamp] = strtotime("$row[release] 8:45:00");
+		$row[release] = explode('-', $row[release]);
+		$row[path] = "graphs/graphs_$row[name].svg";
+		$row[thumbnail] = "png/$row[name].png";
+		$row[tags] = str_getcsv($row[tags]);
+		$row[formatted_name] = trim( ucwords( preg_replace('/_/', ' ', preg_replace('/,/', ', ', preg_replace('/([=\(\)])/', ' $1 ', $row[name]) ) ) ) );
+		$row[content] = false;
+		if($row[timestamp] < $time) // TIME. Release time is at 8:45am Los Angeles time (11:45am NYC, 5:45pm Paris)
+			$graphs[] = $row;
 	}
 
-	// get graphs in reverse chronological order
-	$globbed = glob("../graphs/*.svg");
-	rsort( $globbed );
-	
-	foreach ($globbed as $key => $file) {
-		$parsed = path_to_components($file);
+	// order graphs // debug: this shouldn't need to happen
+	function anti_chronological($a, $b){
+		if($a[timestamp]===$b[timestamp])
+			return 0;
+		return $a[timestamp] > $b[timestamp] ? -1 : 1;
+	}
+	usort($graphs, 'anti_chronological');
 
-		// skip the ones that shouldn't be released yet. Release time is at 8:45am Los Angeles time (11:45am NYC, 5:45pm Paris) <=> (before work LA, before lunch NYC, after work Paris)
-		if($parsed[release_date] > time())
-			continue;
-
-		echo "<a class='container' href='/$parsed[name]'>" . file_get_contents($file) . '</a>';
-
+	// try and MATCH graph to query
+	foreach ($graphs as $key => $graph){
+		echo "<a class='container' href='/$graph[name]'>" . file_get_contents('../'.$graph[path]) . '</a>';
 	}
 ?>
 
