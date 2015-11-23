7eb9e49588a00ed00b49fbe5fb7c15c671211f86
Sheraff <fpellet@ensc.fr>
2015-11-23 14:31:45 +0100

    prevent listing of directories w/ empty index.php files

--- php_utils.php
+++ php_utils.php
@@ -104,4 +104,20 @@
 		], $string);
 		return $string;
   }
+
+  // add blank index.php files to all directories to prevent listings
+  recurse(".");
+  function recurse($path){
+      foreach(scandir($path) as $o){
+          if($o != "." && $o != ".."){
+              $full = $path . "/" . $o;
+              if(is_dir($full)){
+                  if(!file_exists($full . "/index.php")){
+                      file_put_contents($full . "/index.php", "");
+                  }
+                  recurse($full);
+              }
+          }
+      }
+  }
 ?>
