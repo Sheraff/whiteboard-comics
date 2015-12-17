be39fa432a877f4bf0555f0bc7c7fbc180d5a978
Sheraff <fpellet@ensc.fr>
2015-12-17 16:43:30 +0100

    dont save img if asked not to
    actually, just save is with a '-nokern' filename because most likely
    it's not being kept because it was created before the font was loaded

--- save_img.php
+++ save_img.php
@@ -1,9 +1,14 @@
 <?
   $dataURL = $_POST["dataURL"];
   $name = $_GET["name"];
+  $nosave = $_GET["nosave"]==='true';
   $encodedData = explode(',', $dataURL);
   $encodedData = $encodedData[1];
   $decodedData = base64_decode($encodedData);
-  file_put_contents("png/$name.png", $decodedData);
-  echo "png/$name.png";
+  $filename = $nosave ? "png/$name-nokern.png" : "png/$name.png";
+  file_put_contents($filename, $decodedData);
+  echo $filename;
+
+  if(!$nosave && file_exists("png/$name-nokern.png"))
+    unlink("png/$name-nokern.png");
 ?>
