f0adc8eb1852af4f2a9f9707f3182c5db83b6b7a
Sheraff <fpellet@ensc.fr>
2017-07-25 18:15:31 +0200

    proper rewrite rules

--- svg.php
+++ svg.php
@@ -1,8 +1,11 @@
 <?
 	include 'php_utils.php';
 	$graph = $_GET['graph'];
-	$file = "graphs/$graph";
+	$file = "graphs/graphs_$graph.svg";
 	if(file_exists($file)){
 		echo format_svg(file_get_contents($file));
+	} else {
+		echo "404\n";
+		echo $file;
 	}
 ?>
