073d0baa984a6a46b06d9898b75bae5bf4b57ae9
Sheraff <fpellet@ensc.fr>
2017-11-22 18:07:12 +0100

    Potential bug fix

--- save_img.php
+++ save_img.php
@@ -5,6 +5,7 @@
   $encodedData = explode(',', $dataURL);
   $encodedData = $encodedData[1];
   $decodedData = base64_decode($encodedData);
+  if(!file_exists('./png')) mkdir('./png');
   $filename = $nosave ? "png/$name-nokern.png" : "png/$name.png";
   file_put_contents($filename, $decodedData);
   echo $filename;
