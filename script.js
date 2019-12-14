626d0a290438cae1ee33e34ea5152c197276162b
Sheraff <fpellet@ensc.fr>
2019-12-14 02:35:28 +0100

    cleanup

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 import SVGCard from '/components/Card.js'
-import Alphabet from '/modules/Alphabet.js'
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'))
 }
 
 customElements.define('svg-card', SVGCard)
-
-window.alphabet = new Alphabet()
-window.alphabet.promise.then((alphabet) => {
-	console.log(alphabet.getChar('w'))
-})
-setTimeout(() => {
-	window.alphabet.finish().then(console.log)
-}, 500)
