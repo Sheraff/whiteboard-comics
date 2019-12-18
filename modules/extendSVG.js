09ce37a3ea469f5d6b73262cde293ccde85aec76
Florian Pellet <fpellet@ensc.fr>
2019-12-18 10:04:59 +0100

    correct spelling

--- modules/extendSVG.js
+++ modules/extendSVG.js
@@ -6,7 +6,7 @@ export default function extendSVG() {
 
 	extended = true
 
-	const pythagore = (A, B) => Math.sqrt(Math.pow(A[0] - B[0], 2) + Math.pow(A[1] - B[1], 2))
+	const pythagoras = (A, B) => Math.sqrt(Math.pow(A[0] - B[0], 2) + Math.pow(A[1] - B[1], 2))
 
 	SVGElement.prototype.hasLength = function () {
 		return ['line', 'polyline', 'path'].includes(this.tagName)
@@ -21,13 +21,13 @@ export default function extendSVG() {
 	}
 
 	SVGLineElement.prototype.getTotalLength = function () {
-		return pythagore([this.getAttribute('x1'), this.getAttribute('y1')], [this.getAttribute('x2'), this.getAttribute('y2')])
+		return pythagoras([this.getAttribute('x1'), this.getAttribute('y1')], [this.getAttribute('x2'), this.getAttribute('y2')])
 	}
 
 	SVGPolylineElement.prototype.getTotalLength = function () {
 		return this.getAttribute('points').trim().split(' ')
 			.map(point => point.split(',').map(parseFloat))
-			.reduce((acc, point, index, points) => { return index > 0 && acc + pythagore(points[index - 1], point) }, 0)
+			.reduce((acc, point, index, points) => { return index > 0 && acc + pythagoras(points[index - 1], point) }, 0)
 	}
 
 	SVGGraphicsElement.prototype.getStaticTotalLength = function () {
