4ef5d3e7d335d985b7f299bbafead55ea3e26406
Sheraff <fpellet@ensc.fr>
2019-12-22 16:59:48 +0100

    slightly better layout scenario

--- components/Grid.js
+++ components/Grid.js
@@ -1,60 +1,83 @@
 export default class Grid extends HTMLElement {
 	constructor() {
 		super()
-		this.cards = this.querySelectorAll('svg-card')
-		this.cards.forEach(card => card.addEventListener('click', this.getOnClick(card)))
+		this.cards = {}
+		this.querySelectorAll('svg-card').forEach(card => {
+			const name = card.getAttribute('name')
+			this.cards[name] = card
+			card.addEventListener('click', this.getOnClick(card))
+		})
 		this.placeholder = document.createElement('div')
 		this.placeholder.classList.add('svg-card')
 	}
 
+	attributeChangedCallback(name, oldValue, value) {
+		if(name === 'current' && oldValue !== value) {
+			if(value)
+				this.setCurrent(this.cards[value])
+			else
+				this.setCurrent(this.cards[oldValue])
+		}
+	}
+
+	static get observedAttributes() { return ['current']; }
+
 	getOnClick(card) {
 		return () => {
-			requestAnimationFrame(async () => {
-				if(card.SVGAnim.playing)
-					card.SVGAnim.pause()
-
-				await card.ReadyNode
-				await card.eraseAnim.stack.promise
-				card.eraseAnim.play()
-				await card.eraseAnim.promise
-
-				const animation = await this.toggleAnimation(card, () => {
-					if (card.dataset.top)
-						this.removeChild(this.placeholder)
-					if (card.dataset.top)
-						delete card.dataset.top
-					else
-						card.dataset.top = true
-					if (card.dataset.top)
-						this.insertBefore(this.placeholder, card)
-				})
-				
-				if (this.lastPlayed && this.lastPlayed !== card)
-					delete this.lastPlayed.dataset['lastPlayed']
-				this.lastPlayed = card
-				this.lastPlayed.dataset['lastPlayed'] = true
-
-				await Promise.all([
-					new Promise(resolve => animation.onfinish = resolve)
-						.then(() => {
-							card.Alphabet.finish()
-							card.SVGAnim.stack.finish()
-						}),
-					Promise.all([
-						card.Alphabet.promise,
-						card.SVGAnim.stack.promise.then(card.SVGAnim.prepare),
-					]).then(card.eraseAnim.prepare),
-				])
-
-				card.SVGAnim.play()
-
-			})
+			if(this.attributes.current && this.attributes.current.value === card.attributes.name.value)
+				this.setAttribute('current', '')
+			else
+				this.setAttribute('current', card.attributes.name.value)
 		}
+	}
+
+	setCurrent(card) {
+		requestAnimationFrame(async () => {
+			if(card.SVGAnim.playing)
+				card.SVGAnim.pause()
+
+			await card.ReadyNode
+			await card.eraseAnim.stack.promise
+			card.eraseAnim.play()
+			await card.eraseAnim.promise
+
+			const animation = await this.toggleAnimation(card, () => {
+				if (card.dataset.top)
+					this.removeChild(this.placeholder)
+				if (card.dataset.top)
+					delete card.dataset.top
+				else
+					card.dataset.top = true
+				if (card.dataset.top) {
+					this.placeholder[card.attributes.featured ? 'setAttribute' : 'removeAttribute']('featured', true)
+					this.insertBefore(this.placeholder, card)
+				}
+			})
+			
+			if (this.lastPlayed && this.lastPlayed !== card)
+				delete this.lastPlayed.dataset['lastPlayed']
+			this.lastPlayed = card
+			this.lastPlayed.dataset['lastPlayed'] = true
+
+			await Promise.all([
+				new Promise(resolve => animation.onfinish = resolve)
+					.then(() => {
+						card.Alphabet.finish()
+						card.SVGAnim.stack.finish()
+					}),
+				Promise.all([
+					card.Alphabet.promise,
+					card.SVGAnim.stack.promise.then(card.SVGAnim.prepare),
+				]).then(card.eraseAnim.prepare),
+			])
+
+			card.SVGAnim.play()
+
+		})
 	}
 
 	async toggleAnimation(card, callback) {
 		const before = card.getBoundingClientRect()
-		// await new Promise(resolve => requestAnimationFrame(resolve))
 		callback()
 		const after = card.getBoundingClientRect() // TODO: might not need this bc we can know it beforehand
 
