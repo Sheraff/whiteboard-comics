a6b4c5d93b72f9e45e64608ef74b82fb0fc8b93d
Florian Pellet <fpellet@ensc.fr>
2020-03-12 16:34:53 +0100

    only register service worker is not previously installed

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ import SVGCard from './components/Card.js'
 import GridLayout from './components/Grid.js'
 // import NotificationPermission from './modules/NotificationPermission.js'
 
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'))
-}
+if ('serviceWorker' in navigator && !navigator.serviceWorker.controller)
+	navigator.serviceWorker.register('/sw.js', { scope: './' })
 
 customElements.define('svg-card', SVGCard)
 customElements.define('grid-layout', GridLayout)
