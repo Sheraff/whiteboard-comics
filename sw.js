b09dc88a60d1ee418c51afebf3be2c6fd1d003b6
Sheraff <fpellet@ensc.fr>
2020-04-12 21:00:30 +0200

    add new file to SW cache list

--- sw.js
+++ sw.js
@@ -14,6 +14,7 @@ const URLS = [
 	// '/functions/extendSVG.js',
 	// '/functions/hex2hsl.js',
 	// '/functions/SVGToPNG.js',
+	// '/functions/makeSingleton.js',
 	// '/interfaces/IdleNetwork.js',
 	// '/interfaces/IndexedDB.js',
 	// '/interfaces/SWState.js',
